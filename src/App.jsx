e43db124eada03a95ca1729b181f7f456bcd331a
Dario Lo Forti <lofortidario@gmail.com>
2024-06-20 18:04:04 +0200

    Refactor Form component to use axios for API requests and fix create form bug

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,9 @@ export default function() {
             <Form
             tags={tags}
             categories={categories}
-            onCreate={() => fetchPosts()}
+            onCreate={() =>{
+                setShowCreateForm(false);
+                 fetchPosts()}}
             />
         }
             <Posts 
